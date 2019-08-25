9a558e0537d080bfcf822961b0202f6dbc28c619
Rainspistols <ffly@mail.ru>
2019-08-25 16:24:41 +0200

    commend nav.js

--- src/js/nav.js
+++ src/js/nav.js
@@ -6,25 +6,25 @@ function myFunction(x) {
 }
 
 // For Andrew
-const menu = {
-  switchBrs(x) {
-    console.log(x);
-    // const bars = x.querySelectorAll('');
-    // bars.forEach((bar) => {
-    //   bar.classList.toggle("change");
-    // });
-  },
+// const menu = {
+//   switchBrs(x) {
+//     console.log(x);
+//     // const bars = x.querySelectorAll('');
+//     // bars.forEach((bar) => {
+//     //   bar.classList.toggle("change");
+//     // });
+//   },
 
-  init() {
-    const btn  = document.querySelector('.main-btn__sandwitch');
-    btn.addEventListener('click', (evt) => {
-      console.log(evt);
-      this.switchBrs(evt.target);
-    });
-  }
-}
+//   init() {
+//     const btn  = document.querySelector('.main-btn__sandwitch');
+//     btn.addEventListener('click', (evt) => {
+//       console.log(evt);
+//       this.switchBrs(evt.target);
+//     });
+//   }
+// }
 
-document.addEventListener("DOMContentLoaded", function() {
-  console.log('start')
-  menu.init();
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log('start')
+//   menu.init();
+// });
