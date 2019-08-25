12364238ac9c2d5d48c9a3c0d1f537784b57d5c1
Rainspistols <ffly@mail.ru>
2019-08-25 14:02:07 +0200

    suka proebal comity

---
+++ src/js/nav.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function myFunction(x) {
+  x.classList.toggle("change");
+  console.log(x);
+}
+
+// For Andrew
+// const menu = {
+//   switchBrs(x) {
+//     console.log(x);
+//     // const bars = x.querySelectorAll('');
+//     // bars.forEach((bar) => {
+//     //   bar.classList.toggle("change");
+//     // });
+//   }, 
+
+//   init() {
+//     const btn  = document.querySelector('.main-btn__sandwitch');
+//     btn.addEventListener('click', (evt) => {
+//       console.log(evt);
+//       this.switchBrs(evt.target);
+//     });
+//   }
+// }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   console.log('start')
+//   menu.init();
+// });
