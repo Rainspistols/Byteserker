3391b0aec3bff5a538f00eddee5f6a879eeeaf24
Rainspistols <ffly@mail.ru>
2019-09-03 16:47:16 +0200

    finished desktop + small fixes

--- src/js/nav.js
+++ src/js/nav.js
@@ -2,30 +2,4 @@
 
 function myFunction(x) {
   x.classList.toggle("change");
-  console.log(x);
 }
-
-// For Andrew
-const menu = {
-  switchBrs(x) {
-    console.log(x);
-    // const bars = x.querySelectorAll('');
-    // bars.forEach((bar) => {
-    //   bar.classList.toggle("change");
-    // });
-  },
-
-  init() {
-    const btn  = document.querySelector('.main-btn__sandwitch');
-    btn.addEventListener('click', (evt) => {
-      console.log(evt);
-      this.switchBrs(evt.target);
-    });
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-  console.log('start')
-  menu.init();
-});
- 
